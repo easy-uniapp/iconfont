3c73c54028b6da9688e3a0b733e692009e45563b
godtail <godtail@outlook.com>
2020-10-19 14:16:13 +0800

    feat: first release

--- src/commands/iconfont.ts
+++ src/commands/iconfont.ts
@@ -1,12 +1,58 @@
 #!/usr/bin/env node
 
-import chalk from "chalk";
-import path from "path";
+import fs from 'fs'
+import fetch from 'node-fetch'
+import prettier from 'prettier'
 
-console.log(`
-使用说明: iconfont <Font class url>
+const main = async () => {
+  const [_exec, _file, fontClassUrl] = process.argv
 
-- 在 iconfont 我的项目能够看到 Font class url（如 //at.alicdn.com/t/font_2103601_hmqduz3ijg8.css）
-- 导入后，会自动生成 iconfont 样式文件（src/styles/iconfont.scss）
-- 使用时，添加样式 iconfont-xxx 即可
-`);
+  if (!fontClassUrl) {
+    console.log('Usage: iconfont <Font class url>')
+    return
+  }
+
+  try {
+    const ret = await fetch('http:' + fontClassUrl)
+    const text = await ret.text()
+    // get woff2
+    const woff2Matches = text.match(/url.*?format\('woff2'\)/)
+    if (!woff2Matches) {
+      console.error('Error foramt, please check font class url is correct')
+      return
+    }
+    // get iocons
+    const iconMatches = text.match(/.+:before \{[\s\S]*?\}/g)
+    if (!iconMatches) {
+      console.error('No icon found, please check font class url is correct')
+      return
+    }
+    // generate iconfont
+    const generateIconfontScss = `
+    @font-face {
+      font-family: "iconfont";
+      src: ${woff2Matches[0]};
+    }
+    .icon {
+      font-family: "iconfont" !important;
+    }
+    ${iconMatches.map((icon) => icon.replace(/^\..*?-/, '.icon-')).join('\n')}
+    `
+    // prettier format (think about whether it's necessary)
+    const iconfontScss = prettier.format(generateIconfontScss, {
+      parser: 'scss',
+    })
+    // output to src/styles/iconfont.scss
+    fs.writeFile('src/styles/iconfont.scss', iconfontScss, { flag: 'wx' }, (err) => {
+      if (err) {
+        console.error(err?.message)
+        return
+      }
+      console.log('iconfont.scss was genrated in src/styles directory')
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+main()
